669a216415952bbe8d2f3f3774580ef6b4709fd4
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-03-02 23:38:55 +0530

    add delete API test

--- src/main/java/com/example/debtmatesbe/service/DebtService.java
+++ src/main/java/com/example/debtmatesbe/service/DebtService.java
@@ -5,6 +5,8 @@ import com.example.debtmatesbe.repo.DebtRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class DebtService {
     @Autowired
@@ -12,6 +14,15 @@ public class DebtService {
 
     public boolean saveUser(User user) {
         return debtRepo.save(user).getId() != null;
+    }
+
+    public boolean deleteUserById(Long id) {
+        Optional<User> user = debtRepo.findById(id);
+        if (user.isPresent()) {
+            debtRepo.deleteById(id);
+            return true;
+        }
+        return false;
     }
 
 }
